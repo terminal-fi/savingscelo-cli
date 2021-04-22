0287333e9b46a85083c778928463a3e26a01e1bc
Zviad Metreveli <thezviad@gmail.com>
2021-04-22 08:08:39 -0400

    remove unused import

--- src/cli.ts
+++ src/cli.ts
@@ -219,7 +219,7 @@ program
 	.command("rate")
 	.description("Display current CELO / sCELO conversion rate")
 	.action(async () => {
-		const {kit, savingsKit} = await initKit()
+		const {savingsKit} = await initKit()
 		const rate = await savingsKit.celoToSavings(1e18)
 		console.info(`1 CELO = ${rate.shiftedBy(-18).toString(10)} sCELO`)
 	})
